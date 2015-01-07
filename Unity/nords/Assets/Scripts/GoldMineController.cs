bb032e5e2e64acaf1aa302a83ab7f38fdd3b3f7c
mattschwartz <mattschwartz@utexas.edu>
2015-01-06 23:50:19 -0600

    Testing gui stuff

---
+++ Unity/nords/Assets/Scripts/GoldMineController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoldMineController : MonoBehaviour 
+{
+    private bool Focused = false;
+    private bool Over = false;
+
+    public SpriteRenderer Renderer;
+    public Text Txt;
+
+    void FixedUpdate() 
+    {
+        if (Focused || Over) {
+            Renderer.color = Color.white;
+            Txt.text = "Gold Mine";
+        } else {
+            Renderer.color = new Color(0.75f, 0.75f, 0.75f, 1);
+            Txt.text = "Nothing";
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Focused = !Focused;
+    }
+
+	void OnMouseEnter()
+    {
+        Over = true;
+    }
+
+    void OnMouseExit()
+    {
+        Over = false;
+    }
+}
---
+++ Unity/nords/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    void Update() 
+    {
+        // if (Input.GetMouseButtonDown(0)) {
+        //     Vector3 pos = Input.mousePosition;
+        //     pos.z = Camera.main.transform.position.z;
+        //     pos = Camera.main.ScreenToWorldPoint(pos);
+
+        //     RaycastHit2D hit;
+
+        //     hit = Physics2D.Raycast(pos, -Vector2.up);
+
+        //     if (hit.collider != null) {
+        //          Debug.Log("Clicked on " + hit.collider.gameObject.name);
+        //     }
+        // }
+    }
+}
