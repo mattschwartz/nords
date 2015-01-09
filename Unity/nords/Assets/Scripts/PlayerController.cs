a6e9409dec8c57f043285199a42636373c8760a7
mattschwartz <mattschwartz@utexas.edu>
2015-01-08 22:19:55 -0600

    oooh

--- Unity/nords/Assets/Scripts/PlayerController.cs
+++ Unity/nords/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,34 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-    void Update() 
-    {
-        // if (Input.GetMouseButtonDown(0)) {
-        //     Vector3 pos = Input.mousePosition;
-        //     pos.z = Camera.main.transform.position.z;
-        //     pos = Camera.main.ScreenToWorldPoint(pos);
+	public float Speed = 7;
+	public float ScrollSpeed = 28;
+	public float RotateSpeed = 50;
+	public Transform CameraBoom;
 
-        //     RaycastHit2D hit;
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Q)) {
+			CameraBoom.Rotate(new Vector3(0, -RotateSpeed * Time.deltaTime, 0));
+		}
 
-        //     hit = Physics2D.Raycast(pos, -Vector2.up);
+		if (Input.GetKey (KeyCode.E)) {
+			CameraBoom.Rotate(new Vector3(0, RotateSpeed * Time.deltaTime, 0));
+		}
 
-        //     if (hit.collider != null) {
-        //          Debug.Log("Clicked on " + hit.collider.gameObject.name);
-        //     }
-        // }
-    }
+		float hMove = Input.GetAxis("Horizontal");
+		float vMove = Input.GetAxis("Vertical");
+		float yMove = Input.GetAxis("Mouse ScrollWheel");
+
+		var vec = new Vector3(hMove * Speed * Time.deltaTime, 0, vMove * Speed * Time.deltaTime);
+		
+		CameraBoom.Translate(vec);
+
+		if (Mathf.Abs(yMove) > 0) {
+			var rot = Quaternion.Euler(45, 0, 0);
+			vec = rot * new Vector3(0, 0, yMove * ScrollSpeed * Time.deltaTime);
+
+			CameraBoom.Translate(vec);
+		}
+	}
 }
